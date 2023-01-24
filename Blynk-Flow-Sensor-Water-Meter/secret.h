a262413d4d23544a92b64ab1cfa3a4579a672485
CastieldSl <emrebuz2222@gmail.com>
2023-01-24 15:03:06 +0300

    BlynkCodes
    Codes

---
+++ Blynk-Flow-Sensor-Water-Meter/secret.h
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////// 
+//             Secret Configuration                      //
+////////////////////////////////////////////////////////// 
+
+
+/***************************************************
+         Blynk Settings
+ **************************************************/
+
+#define AUTH "auth token buraya"                            // You should get Auth Token in the Blynk App.  
+
+/***************************************************
+          WiFi Settings
+ **************************************************/
+
+#define WIFI_SSID "wifi adını buraya yaz"                                    
+#define WIFI_PASS "wifi şifreni buraya yaz"                                   
+
+/***************************************************
+          Server Settings
+ **************************************************/
+
+#define SERVER "xxx.xxx.xx.xxx"                                 // Comment-out if use Blynk hosted cloud service
+
+#define PORT 8080
